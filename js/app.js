4a82dde1ee693030f93e3e10cfb206a73d620bca
Daniel Nelmes-Love <dnelmeslove@gmail.com>
2015-01-04 11:47:46 +1300

    changed html ids for readability; changed layout and size of elements

--- js/app.js
+++ js/app.js
@@ -1,8 +1,4 @@
 $(document).on('ready', function() {
-var	n = 1,
-	x = 1,
-	y = 1;
-
 	resetQuiz();
 	clickButton();
 });
@@ -10,8 +6,8 @@ var	n = 1,
 var clickButton = function() {
 	$("#submit").on("mousedown", function() {
 		if (n <= 5) {
-			$("#q-no").text("Question " + n + ":");
-			$("#p-no").text(n + " of 5");
+			$("#questionNumber").text("Question " + n + ":");
+			$("#progressNumber").text(n + " of 5");
 			myCard();
 			oppCard();
 			n++;
@@ -20,6 +16,12 @@ var clickButton = function() {
 		}
 	});
 }
+
+// Quiz
+
+
+
+// Minigame
 
 var myCard = function() {
 	if (true) {
@@ -35,23 +37,25 @@ var oppCard = function() {
 	cardChange("#oppCard", n, "opponent");
 }
 
-function cardChange (node, variable, alt) {
+var cardChange =function(node, variable, alt) {
 	$("#cp" + n).remove();
 	$(node).prepend('<img src="img/' + alt + '/' + variable + '.png" alt="' + alt + '" class="card">');
 }
+
+// Reset Quiz
 
 var resetQuiz = function() {
 	n=1;
 	x=1;
 	y=1;
 
-	$("#q-no").text("Questions");
-	$("#p-no").text("0 of 5");
-	cardReset("#myCard");
-	cardReset("#oppCard");
+	$("#questionNumber").text("Questions");
+	$("#progressNumber").text("0 of 5");
+	mgReset("#myCard");
+	mgReset("#oppCard");
 }
 
-var cardReset = function(node) {
+var mgReset = function(node) {
 	$(node).children().remove();
 	for (var i = 1; i <= 5; i++) {
 		$(node).append('<img src="img/ph.png" alt="placeholder" id="cp' + i + '" class="card">');
