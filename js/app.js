c3ea40f105e2336ef4bb3d14f39f84952e983d16
Daniel Nelmes-Love <dnelmeslove@gmail.com>
2015-01-04 00:33:50 +1300

    created functions to update myCard on true

--- js/app.js
+++ js/app.js
@@ -1,15 +1,45 @@
 $(document).on('ready', function() {
-
 	clickButton();
-
 });
 
-var n = 1
+var	n = 1,
+	x = 1,
+	y = 1;
 
 var clickButton = function() {
 	$("#submit").on("mousedown", function() {
-		n++;
-		$("#q-no").text("Question " + n + ":");
-		$("#p-no").text(n + " of 5");
+		if (n <= 5) {
+			$("#q-no").text("Question " + n + ":");
+			$("#p-no").text(n + " of 5");
+			myCard();
+			n++;
+		} else {
+			resetQuiz()
+		}
 	});
+}
+
+var myCard = function() {
+	if (prompt()) {
+		$("#cp" + n).remove();
+		$("#myCard").prepend('<img src="img/correct/' + x + '.png" alt="correct" id="cp' + x + '" class="card">')
+		x++;
+	} else {
+		$("#cp" + n).remove();
+		$("#myCard").prepend('<img src="img/incorrect/' + y + '.png" alt="incorrect" id="cp' + y + '" class="card">')
+		y++;
+	}
+}
+
+var resetQuiz = function() {
+	n=1;
+	x=1;
+	y=1;
+
+	$("#q-no").text("Questions");
+	$("#p-no").text("0 of 5");
+	$("#myCard").children().remove();
+	for (var i = 1; i <= 5; i++) {
+		$("#myCard").append('<img src="img/ph.png" alt="placeholder" id="cp' + i + '" class="card">')
+	};
 }
