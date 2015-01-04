2e7077259775a4e7345cd196fc1179fffa022bb0
Daniel Nelmes-Love <dnelmeslove@gmail.com>
2015-01-04 13:53:56 +1300

    fixed counter issues; added functions for examples and end

--- js/app.js
+++ js/app.js
@@ -6,10 +6,12 @@ $(document).on('ready', function() {
 var clickButton = function() {
 	$("#submit").on("mousedown", function() {
 		if (n <= 5) {
-			$("#questionNumber").text("Question " + n + ":");
-			$("#progressNumber").text(n + " of 5");
-			myCard();
-			oppCard();
+			quizQuestion();
+			minigame();
+			n++;
+		} else if (n===6) {
+			endQuiz();
+			minigame();
 			n++;
 		} else {
 			resetQuiz()
@@ -18,41 +20,70 @@ var clickButton = function() {
 }
 
 // Quiz
+var quizQuestion = function() {
+	var	z = (n-1),
+		q1 = "Question 1",
+		q2 = "Question 2",
+		q3 = "Question 3",
+		q4 = "Question 4",
+		q5 = "Question 5",
+		questionsArray = [q1, q2, q3, q4, q5];
+	updateProgress("Question:", n + " of 5");
+	$("#questions").append('<p class="q">' + questionsArray[z] + '</p>');
+//	quizOptions();
+	quizExamples();
+}
 
+var updateProgress = function(head, body) {
+	$(".q").remove();
+	$("#progressTitle").text(head);
+	$("#progressNumber").text(body);
+}
 
+var quizExamples = function() {
+	$(".exampleImage").remove();
+	$("#examples").append('<img src="img/examples/' + n + '.jpg" alt="example ' + n + '" class="exampleImage">');
+	console.log("image " + n)
+}
+
+var endQuiz = function() {
+	if (true) {
+		updateProgress("Game over", "You win!");
+	} else {
+		updateProgress("Game over", "You lose...");
+	};
+}
 
 // Minigame
 
-var myCard = function() {
+var minigame = function() {
 	if (true) {
 		cardChange("#myCard", x, "correct");
 		x++;
+		y++;
 	} else {
 		cardChange("#myCard", y, "incorrect");
 		y++
-	}
+	};
+	cardChange("#oppCard", z, "opponent");
+	z++
 }
 
-var oppCard = function() {
-	cardChange("#oppCard", n, "opponent");
-}
-
-var cardChange =function(node, variable, alt) {
-	$("#cp" + n).remove();
-	$(node).prepend('<img src="img/' + alt + '/' + variable + '.png" alt="' + alt + '" class="card">');
+var cardChange = function(node, variable, source) {
+	if (n > 1) {
+		$("#cp" + (n-1)).remove();
+		$(node).prepend('<img src="img/' + source + '/' + variable + '.png" alt="' + source + '" class="card">');
+	};
 }
 
 // Reset Quiz
 
 var resetQuiz = function() {
-	n=1;
-	x=1;
-	y=1;
-
-	$("#questionNumber").text("Questions");
-	$("#progressNumber").text("0 of 5");
+	n = 1;
+	x = y = z = 0;
 	mgReset("#myCard");
 	mgReset("#oppCard");
+	updateProgress("", "")
 }
 
 var mgReset = function(node) {
@@ -60,4 +91,4 @@ var mgReset = function(node) {
 	for (var i = 1; i <= 5; i++) {
 		$(node).append('<img src="img/ph.png" alt="placeholder" id="cp' + i + '" class="card">');
 	};
-};
+}
