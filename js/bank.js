bf94fa738a256c4ac028de0b58aef803e504f841
Daniel Nelmes-Love <dnelmeslove@gmail.com>
2015-01-21 22:57:16 +1300

    Reworked example images
    Renamed existing example images.
    Added new questions, options and example images to the bank.

--- js/bank.js
+++ js/bank.js
@@ -1,24 +1,44 @@
 function questionArray() {
 	questionBank = [
-		{	question: "In Five Card Poker would a Royal Flush beat 4 Aces?",
-			possibleOptions: [{text: "Yes", val: 0},{text: "No", val: 0}, {text: "They would draw", val: 0}, {text: "Those two hands can't happen together", val: 1}],
-			image: 1
+		{	question: "In Roulette what is NOT a betting option?",
+			possibleOptions: [{text: "White", val: 1},{text: "Black", val: 0}, {text: "1st 12", val: 0}, {text: "19 to 36", val: 0}],
+			image: "roulette"
 		},
-		{	question: "In Blackjack what's the term used if a player scores over 21?",
-			possibleOptions: [{text: "Blackjack", val: 0},{text: "Hit", val: 0}, {text: "Bust", val: 1}, {text: "Full House", val: 0}],
-			image: 2
+		{	question: "A player does what to give up in a round of poker?",
+			possibleOptions: [{text: "Fold", val: 1},{text: "Bluff", val: 0}, {text: "Check", val: 0}, {text: "Call", val: 0}],
+			image: "badhand"
 		},
 		{	question: "What is the term used when two ones are rolled in Casino Craps?",
 			possibleOptions: [{text: "Rat Eyes", val: 0},{text: "Snake Eyes", val: 1}, {text: "Ace Deuce", val: 0}, {text: "Double Ones", val: 0}],
-			image: 3
+			image: "dice"
+		},
+		{	question: "Who is the actor that plays Danny Ocean in Oceans Eleven?",
+			possibleOptions: [{text: "Bernie Mac", val: 0},{text: "George Clooney", val: 1}, {text: "Brad Pitt", val: 0}, {text: "Matt Damon", val: 0}],
+			image: "eleven"
+		},
+		{	question: "In Blackjack what's the term used if a player scores over 21?",
+			possibleOptions: [{text: "Blackjack", val: 0},{text: "Hit", val: 0}, {text: "Bust", val: 1}, {text: "Full House", val: 0}],
+			image: "blackjack"
 		},
 		{	question:  "In Texas Hold'em what are your two starting cards called?",
 			possibleOptions: [{text: "Box cards", val: 0},{text: "House cards", val: 0}, {text: "Hole cards", val: 1}, {text: "River cards", val: 0}],
-			image: 4
+			image: "holdem"
 		},
-		{	question: "In Roulette what is NOT a betting option",
-			possibleOptions: [{text: "White", val: 1},{text: "Black", val: 0}, {text: "1st 12", val: 0}, {text: "19 to 36", val: 0}],
-			image: 5
+		{	question:  "In a common phrase what is \"chicken dinner\" preceded by?",
+			possibleOptions: [{text: "Spinner winner...", val: 0},{text: "Picken winner...", val: 0}, {text: "Winner winner...", val: 1}, {text: "Theres no such phrase", val: 0}],
+			image: "chicken"
+		},
+		{	question: "In Five Card Poker would a Royal Flush beat 4 Aces?",
+			possibleOptions: [{text: "Yes", val: 0},{text: "No", val: 0}, {text: "They would draw", val: 0}, {text: "Those two hands can't happen together", val: 1}],
+			image: "royalflush"
+		},
+		{	question: "What did the characters in 21 become experts in?",
+			possibleOptions: [{text: "Dice rolling", val: 0}, {text: "Dice reading", val: 0}, {text: "Card shuffling", val: 0}, {text: "Card counting", val: 1}],
+			image: 21
+		},
+		{	question: "Which of the following colours are never used for chips?",
+			possibleOptions: [{text: "Pink", val: 0},{text: "Brown", val: 0}, {text: "Yellow", val: 0}, {text: "They can all be used", val: 1}],
+			image: "chips"
 		}
 	]
 };
